8eb6ae08f5be5cbf5b3dc86472b2fcfd24218a25
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2021-01-22 15:49:38 +0900

    add util func

--- pkg/common/util.go
+++ pkg/common/util.go
@@ -1,9 +1,30 @@
 package common
 
+import (
+	"time"
+
+	"github.com/CyberAgent/mimosa-google/proto/google"
+)
+
 // CutString returns cutting specific `cut` characters with ` ...` suffix from `input` string.
 func CutString(input string, cut int) string {
 	if len(input) > cut {
 		return input[:cut] + " ..." // cut long text
 	}
 	return input
+}
+
+// InitScanStatus return init AttachGCPDataSourceRequest data
+func InitScanStatus(g *google.GCPDataSource) *google.AttachGCPDataSourceRequest {
+	return &google.AttachGCPDataSourceRequest{
+		ProjectId: g.ProjectId,
+		GcpDataSource: &google.GCPDataSourceForUpsert{
+			GcpId:              g.GcpId,
+			GoogleDataSourceId: g.GoogleDataSourceId,
+			ProjectId:          g.ProjectId,
+			ScanAt:             time.Now().Unix(),
+			Status:             google.Status_UNKNOWN, // After scan, will be updated
+			StatusDetail:       "",
+		},
+	}
 }
