17c827324e4f657fd896e29e8e12d1c8388241e3
mmmknt <mmmknt@mmmknt.dev>
2022-02-17 13:21:21 +0900

    Make assign parameters to main

--- src/asset/main.go
+++ src/asset/main.go
@@ -9,12 +9,36 @@ import (
 	"github.com/gassara-kys/envconfig"
 )
 
-type serviceConfig struct {
+type AppConfig struct {
 	EnvName string `default:"local" split_words:"true"`
+
+	// asset
+	GoogleCredentialPath string `required:"true" split_words:"true" default:"/tmp/credential.json"`
+
+	// grpc
+	FindingSvcAddr string `required:"true" split_words:"true" default:"finding.core.svc.cluster.local:8001"`
+	AlertSvcAddr   string `required:"true" split_words:"true" default:"alert.core.svc.cluster.local:8004"`
+	GoogleSvcAddr  string `required:"true" split_words:"true" default:"google.google.svc.cluster.local:11001"`
+
+	// sqs
+	Debug string `default:"false"`
+
+	AWSRegion   string `envconfig:"aws_region"   default:"ap-northeast-1"`
+	SQSEndpoint string `envconfig:"sqs_endpoint" default:"http://queue.middleware.svc.cluster.local:9324"`
+
+	AssetQueueName     string `split_words:"true" default:"google-asset"`
+	AssetQueueURL      string `split_words:"true" default:"http://queue.middleware.svc.cluster.local:9324/queue/google-asset"`
+	MaxNumberOfMessage int64  `split_words:"true" default:"10"`
+	WaitTimeSecond     int64  `split_words:"true" default:"20"`
+
+	// handler
+	WaitMilliSecPerRequest int `split_words:"true" default:"500"`
+	AssetAPIRetryNum       int `split_words:"true" default:"3"`
+	AssetAPIRetryWaitSec   int `split_words:"true" default:"30"`
 }
 
 func main() {
-	var conf serviceConfig
+	var conf AppConfig
 	err := envconfig.Process("", &conf)
 	if err != nil {
 		appLogger.Fatal(err.Error())
@@ -23,13 +47,33 @@ func main() {
 	if err != nil {
 		appLogger.Fatal(err.Error())
 	}
+
 	appLogger.Info("Start")
-	ctx := context.Background()
-	consumer := newSQSConsumer()
+	handler := &sqsHandler{
+		waitMilliSecPerRequest: conf.WaitMilliSecPerRequest,
+		assetAPIRetryNum:       conf.AssetAPIRetryNum,
+		assetAPIRetryWaitSec:   conf.AssetAPIRetryWaitSec,
+	}
+	handler.findingClient = newFindingClient(conf.FindingSvcAddr)
+	handler.alertClient = newAlertClient(conf.AlertSvcAddr)
+	handler.googleClient = newGoogleClient(conf.GoogleSvcAddr)
+	handler.assetClient = newAssetClient(conf.GoogleCredentialPath)
+
+	sqsConf := &sqsConfig{
+		Debug:              conf.Debug,
+		AWSRegion:          conf.AWSRegion,
+		SQSEndpoint:        conf.SQSEndpoint,
+		AssetQueueName:     conf.AssetQueueName,
+		AssetQueueURL:      conf.AssetQueueURL,
+		MaxNumberOfMessage: conf.MaxNumberOfMessage,
+		WaitTimeSecond:     conf.WaitTimeSecond,
+	}
+	consumer := newSQSConsumer(sqsConf)
 	appLogger.Info("Start the SQS consumer server for GCP Cloud Asset Inventory...")
+	ctx := context.Background()
 	consumer.Start(ctx,
 		mimosasqs.InitializeHandler(
 			mimosasqs.RetryableErrorHandler(
 				mimosasqs.StatusLoggingHandler(appLogger,
-					mimosaxray.MessageTracingHandler(conf.EnvName, "google.asset", newHandler())))))
+					mimosaxray.MessageTracingHandler(conf.EnvName, "google.asset", handler)))))
 }
