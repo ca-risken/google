4a5a2ebdc0a9518c7058eec538415ac9bd0836a4
mmmknt <mmmknt@mmmknt.dev>
2022-07-26 20:21:43 +0900

    Return error when failed to create sqs client

--- src/cloudsploit/main.go
+++ src/cloudsploit/main.go
@@ -121,7 +121,10 @@ func main() {
 		MaxNumberOfMessage: conf.MaxNumberOfMessage,
 		WaitTimeSecond:     conf.WaitTimeSecond,
 	}
-	consumer := newSQSConsumer(ctx, sqsConf)
+	consumer, err := newSQSConsumer(ctx, sqsConf)
+	if err != nil {
+		appLogger.Fatalf(ctx, "Failed to create SQS consumer, err=%+v", err)
+	}
 
 	appLogger.Info(ctx, "Start the SQS consumer server for GCP CloudSploit...")
 	consumer.Start(ctx,
