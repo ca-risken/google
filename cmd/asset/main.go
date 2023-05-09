132e6dd2c5b37cbf36b7a98340cd3423afd1ff5d
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-05-09 11:36:19 +0900

    fix: datasource-api

--- cmd/asset/main.go
+++ cmd/asset/main.go
@@ -38,7 +38,7 @@ type AppConfig struct {
 
 	// grpc
 	CoreSvcAddr          string `required:"true" split_words:"true" default:"core.core.svc.cluster.local:8080"`
-	DataSourceAPISvcAddr string `required:"true" split_words:"true" default:"datasource-api.core.svc.cluster.local:8081"`
+	DataSourceAPISvcAddr string `required:"true" split_words:"true" default:"datasource-api.datasource.svc.cluster.local:8081"`
 
 	// sqs
 	Debug string `default:"false"`
